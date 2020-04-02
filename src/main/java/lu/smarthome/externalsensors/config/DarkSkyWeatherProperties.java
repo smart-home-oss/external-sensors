e89f763964a36c0ce8dc2fe9e26930ce45af1128
Ionut Mihai Sandu <rowebsoft@gmail.com>
2020-04-02 20:49:18 +0200

    last commit

--- src/main/java/lu/smarthome/externalsensors/config/DarkSkyWeatherProperties.java
+++ src/main/java/lu/smarthome/externalsensors/config/DarkSkyWeatherProperties.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class DarkSkyWeatherProperties {
 
+
     private String apiUrl;
     private String appId;
     private String latitude = "49.797955";
